54752d849a687a85cd0e41e3fe6d4db37d775216
GuilhermeHorta22 <ghorta800@gmail.com>
2025-08-23 11:17:36 -0300

    TAD finalizado

--- TAD.h
+++ TAD.h
@@ -27,7 +27,7 @@ void inserirInicio(descritor *desc, int info)
 	novo->info = info;
 	novo->ant = NULL;
 	novo->prox = desc->inicio;
-	if(desc->inicio == NULL)
+	if(isEmpty(desc))
 		desc->inicio = desc->fim = novo;
 	else
 	{
@@ -42,7 +42,7 @@ void inserirFinal(descritor *descritor, int info)
 	novo->info = info;
 	novo->ant = desc->fim;
 	novo->prox = NULL;
-	if(desc->inicio == NULL)
+	if(isEmpty(desc)) //retorna se está vazio
 		desc->inicio = desc->fim = novo;
 	else
 	{
@@ -53,12 +53,52 @@ void inserirFinal(descritor *descritor, int info)
 
 listaDp *buscar(descritor *descritor, int info)
 {
+	listaDp *aux = desc->inicio;
 	
+	while(aux != NULL && aux->info != info)
+		aux = aux->prox;
+		
+	if(aux != NULL)
+		return aux;
+	else
+		return NULL;
 }
 
 int exclui(descritor *desc, int info)
 {
+	listaDp *aux;
 	
+	if(!isEmpty(desc))
+	{
+		//buscar o elemento
+		aux = buscar(desc,info);
+		
+		//verificar se achou
+		if(aux != NULL)
+		{
+			if(desc->inicio == aux && desc->fim == aux) //unico elemento na lista
+			{
+				desc->inicio = NULL;
+				desc->fim = NULL;
+			}
+			else
+			if(desc->inicio == aux) //verifico se é no começo
+			{
+				desc->inicio = desc->inicio->prox;
+				desc->inicio->ant = NULL;
+			}
+			else
+			if(desc->fim == aux) //verifico se é no final
+			{
+				desc->fim = desc->fim->ant;
+				desc->fim->prox = NULL;
+			}
+			else //verifico se é no meio
+			{
+				aux->ant->prox = aux->prox;
+				aux->prox->ant = aux->ant;
+			}
+			free(aux);
+		}
+	}
 }
-
-
